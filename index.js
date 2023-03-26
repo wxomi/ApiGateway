55f5090a4eeb8de1d45134a8fe7629cac1673f74
Anshul Saini <anshulsaini@Anshuls-MacBook-Air.local>
2023-03-26 14:50:08 +0530

    Added isAdmin for flights service

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const { checkAuthenticationUser, checkIsAdmin } = require("./middlewares");
 
 const app = express();
 
@@ -9,25 +10,37 @@ const PORT = 3005;
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 });
 
 app.use(morgan("combined"));
-// Apply the rate limiting middleware to all requests
+// // Apply the rate limiting middleware to all requests
 app.use(limiter);
 app.use(
-  "/bookingservice",
+  "/authservice",
   createProxyMiddleware({
     target: "http://localhost:3001/",
     changeOrigin: true,
   })
 );
-app.get("/home", (req, res) => {
-  console.log("hi");
-  return res.status(200).json({
-    message: "OK",
-  });
-});
+
+app.use("/bookingservice", checkAuthenticationUser.checkAuthentication);
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: "http://localhost:3002/",
+    changeOrigin: true,
+  })
+);
+
+app.use("/flightservice", checkIsAdmin.checkIsAdmin);
+app.use(
+  "/flightservice",
+  createProxyMiddleware({
+    target: "http://localhost:3000/",
+    changeOrigin: true,
+  })
+);
 app.listen(PORT, () => {
   console.log("Server started at port: ", PORT);
 });
